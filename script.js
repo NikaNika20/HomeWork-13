6d2a576abe0cf6383d5d975c7858a05ebb9b91e9
NikaBeruashvili <nikab562@gmail.com>
2024-10-17 14:49:15 -0700

    did something in console

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+const FirstName = "Nika"
+const LastName = "beruashvili"
+let age = 15
+const meil = "nika@gmail.com"
+const PhoneNumber = "599-12-34-56"
+const isage = false
+
+
+console.log(FirstName)
+console.log(LastName)
+console.log(age)
+console.log(meil)
+
+console.info("%c" + PhoneNumber, "color:red")
+console.info("%c" + isage, " color:yellow")
+
